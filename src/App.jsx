16b39c8805df15ff6cc866e079be6e2dd8b2ccd7
shamashaidergondal <shamasgondal333@gmail.com>
2024-09-15 05:01:16 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./assets/About"
+
+
+function App  ()  {
+
+  return (
+<>
+<About/>  
+<h1>hello world</h1>
+<button onClick={"oppen the page"}></button>
+
+
+</>
+  
+  
+  )
+}
+
+export default (App)
