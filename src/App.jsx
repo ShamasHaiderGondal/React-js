dc20aa0ec7f04be95629e039441cb448c6908151
shamashaidergondal <shamasgondal333@gmail.com>
2024-09-15 05:11:37 -0700

    commit from second project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 const App = () =>{
     return (
+        <>
+        
         <h1>learninh git hub </h1>
+        <h1>learninh git hub </h1>
+        <h1>learninh git hub </h1>
+        <h1>learninh git hub </h1>
+        </>
     )
 }
       
